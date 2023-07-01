2728ed0aa2bafd40d7945a5cfaf836b735ea2726
artemyderik <derry0808@gmail.com>
2023-07-01 23:44:01 +0700

    It works.

---
+++ iNequest/NetData/Links.swift
@@ -0,0 +1,15 @@
+//
+//  NetData.swift
+//  iNequest
+//
+//  Created by Артемий Дериглазов on 01.07.2023.
+//
+
+enum Link: String {
+    case garblovian = "https://rickandmortyapi.com/api/character/133"
+    case glen = "https://rickandmortyapi.com/api/character/145"
+    case millionAnts = "https://rickandmortyapi.com/api/character/226"
+    case florflock = "https://rickandmortyapi.com/api/character/665"
+    case yoYoRick = "https://rickandmortyapi.com/api/character/773"
+    case gotronPilot = "https://rickandmortyapi.com/api/character/777"
+}
